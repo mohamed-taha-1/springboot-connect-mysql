df74abf34fd26eaf0e459a7c25f7cbb95caef77a
mohamed-taha-1 <mohamedtahaabdel@gmail.com>
2021-09-04 07:38:57 +0200

    connect to mysql  - jdbc and mysql

---
+++ src/main/java/com/example/testSql/Controller.java
@@ -0,0 +1,23 @@
+package com.example.testSql;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+
+  @RequestMapping("/insert")
+    public String index(){
+      jdbc.execute("insert into student (Fname , email) values ('mohamed','ta@gmail.com')");
+      return " inserted ";
+
+  }
+
+}
